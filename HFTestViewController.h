b3e94f2f066c1f571649ad49c6031816b0309ee8
zwq <1071942338@qq.com>
2018-10-16 22:27:12 +0800

    添加测试页面

---
+++ HFTestViewController.h
@@ -0,0 +1,16 @@
+//
+//  HFTestViewController.h
+//  HFLibTest
+//
+//  Created by 张文旗 on 2018/10/16.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HFTestViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HFTestViewController.m
@@ -0,0 +1,31 @@
+//
+//  HFTestViewController.m
+//  HFLibTest
+//
+//  Created by 张文旗 on 2018/10/16.
+//
+
+#import "HFTestViewController.h"
+
+@interface HFTestViewController ()
+
+@end
+
+@implementation HFTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
